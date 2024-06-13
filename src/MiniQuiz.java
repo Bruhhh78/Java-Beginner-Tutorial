13a3df5f926089ed38b077775f7885ac7eac6aba
Anmol Srivastava <anmolsrivastava678@gmail.com>
2024-06-13 16:55:20 +0530

    git initialize

--- src/MiniQuiz.java
+++ src/MiniQuiz.java
@@ -1,7 +1,7 @@
 public class MiniQuiz{
     public static void main(String[]args){
         //Simple Practice Question :-  We have to print sum of two value 
-        int a = 1, b= 2;
+        int a = 3, b= 2;
         int sum = a+b;
         System.out.println("5 - This is Mini Quiz file.");
         System.out.println("Value Of Sum is " + sum);
